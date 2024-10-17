3fb6ea299dad3aed11bc621d7c988fd21e336a1d
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-17 16:32:29 +0200

    il primo esercizio fatto

--- script.js
+++ script.js
@@ -3,8 +3,14 @@
 
 
 // creo un Array con la lista degli Email
-const emailList = [ 'mimmo@gmailcom', 'pippo@gmail.com' , 'luigi@gmail.com']
+const emailList = [ 'mimmo@gmail.com', 'pippo@gmail.com' , 'luigi@gmail.com']
 
-// chiedo utente suo l'email con metode prompt
-const emailUtente = prompt("inserisci tuo l'email per favore");
-console. log(emailUtente);
+// chiedo utente la sua email con metode prompt
+const emailUtente = prompt("inserisci la tua email per favore");
+
+// controlliamo se l'mail inserito esiste nella nostra lista
+if (emailList.includes(emailUtente)){ // se email esiste stampo il messagio "Accesso Approvato"
+    console. log("Accesso Approvato");
+}else { // altrimenti se email non concide con la lista nostra stampiamo il messagio "Email errato Accesso Negato"
+    console. log("Email errato Accesso Negato");
+}
