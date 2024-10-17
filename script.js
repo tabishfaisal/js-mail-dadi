8b2c491161567877a3e5d474df17ba6950ff2294
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-17 16:15:34 +0200

    starting ex.1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// creo un Array con la lista degli Email
+const emailList = [ 'mimmo@gmailcom', 'pippo@gmail.com' , 'luigi@gmail.com']
+
+// chiedo utente suo l'email con metode prompt
+const emailUtente = prompt("inserisci tuo l'email per favore");
+console. log(emailUtente);
